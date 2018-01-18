a5d67455a82b48d60311cc08e15d17b450b51293
Peter Pouliot <peter@pouliot.net>
2018-01-18 16:50:21 -0500

    removing bad python line

--- ghe-startup.sh
+++ ghe-startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ -e /root/.ssh/id_rsa ] || [ -e /root/.ssh/id_dsa ] || [ -e /root/.ssh/id_ed25519  ] || [ -e /root/.ssh/id_ecdsa ] || ; then
+if [ -e /root/.ssh/id_rsa ] || [ -e /root/.ssh/id_dsa ] || [ -e /root/.ssh/id_ed25519  ] || [ -e /root/.ssh/id_ecdsa ]; then
   echo -n "SSH Keys Found. Starting SSH_AGENT then GHE"
   eval `ssh-agent` &&  ssh-add ; /ghe-startup.py
 else
