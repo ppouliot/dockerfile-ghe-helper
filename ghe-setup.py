cf3be7d4860539febfa7db5642387d963fa355b0
Peter Pouliot <peter@pouliot.net>
2018-01-18 10:48:49 -0500

    update ghe startup script

--- ghe-setup.py
+++ ghe-setup.py
@@ -1,16 +1,27 @@
 #! /usr/bin/env python
 
-from ghe import GHE
 import os 
+from ghe import get_key, set_key, unset_key
+from ghe import GHE
+print os.environ['GHE_HOST']
+print os.environ['GHE_SSH_USER']
+print os.environ['GHE_SSH_PORT']
+print os.environ['GHE_USER']
+print os.environ['GHE_PASS']
+print os.environ['GHE_TOKEN']
+print os.environ['GH_TOKEN']
+print os.environ['GH_TOTP']
 
-os.environment['GHE_HOST'] = 'ghe-host'
-os.environment['GHE_SSH_USER'] = 'ghe-ssh-user'
-os.environment['GHE_SSH_PORT'] = 'ghe-port'
-os.environment['GHE_USER'] = 'ghe-user'
-os.environment['GHE_PASS'] = 'ghe-pass'
-os.environment['GHE_TOKEN'] = 'ghe-token'
-os.environment['GH_TOKEN'] = 'gh-token'
-os.environment['GHE_TOTP'] = 'ghe-totp'
+from ghe import get_key, set_key, unset_key
+set_key('ghe-host',(os.environ['GHE_HOST']))
+set_key('ghe-ssh-user',(os.environ['GHE_SSH_USER']))
+set_key('ghe-ssh-port',(os.environ['GHE_SSH_PORT']))
+set_key('ghe-user',(os.environ['GHE_USER']))
+set_key('ghe-pass',(os.environ['GHE_PASS']))
+set_key('ghe-token',(os.environ['GHE_TOKEN']))
+set_key('gh-token',(os.environ['GH_TOKEN']))
+set_key('ghe-totp',(os.environ['GHE_TOTP']))
+
 
 app = GHE()
 app.cmdloop()
